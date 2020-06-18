f9abe52a9d5b831aa47b93090f9c8fbcc7d872be
Kumar Ujjawal <ujjawal.kumar@nagra.com>
2020-06-18 15:27:21 +0530

    deleted context file from parent folder

--- IMSComponentContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-const IMSComponentContext=React.createContext()
-
-const IMSComponentRunningProvider=IMSComponentContext.Provider
-const IMSComponentRunningConsumer=IMSComponentContext.Consumer
-
-export  {IMSComponentRunningProvider,IMSComponentRunningConsumer}
